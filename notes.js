c865c324014b5f6aa5bc9fd78a79aa1370635f78
Isaac Weiser <isaac.weiser.x@gmail.com>
2021-11-16 16:23:25 -0500

    first commit

---
+++ notes.js
@@ -0,0 +1,19 @@
+const notes = [
+    {
+        id: 1,
+        subject: "flex box",
+        date: "11-15-21",
+        feeling: "yeet",
+        timeSpent: 6
+    },
+    {
+        id: 2,
+        subject: "data types",
+        date: "11-16-21",
+        feeling: "pog",
+        timeSpent: 2
+    }
+]
+
+
+console.log(notes)
