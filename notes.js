634eed64bee7543ec50f1b4b5e3fc89b2ab5e4ab
Isaac Weiser <isaac.weiser.x@gmail.com>
2021-11-17 14:18:14 -0500

    added a note for 11.17 using for of loop

--- notes.js
+++ notes.js
@@ -17,3 +17,10 @@ const notes = [
 
 
 console.log(notes)
+
+// new note added using push
+
+notes.push({id:3, subject:["push function", "arrays and objects", "for...of loops", "array iteration"], date: "11-17-21", feeling:"somewhat anxious", timeSpent:2.5})
+
+console.log("today's new knowledge: " + JSON.stringify(notes[2]))
+console.log(notes[2])
